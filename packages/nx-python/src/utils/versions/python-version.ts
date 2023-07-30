daed14f45f71b44dd774d4200745e0cdf6f8d257
fbronfravi <ronald.cardenas@forumbrands.com>
2023-07-30 02:18:56 -0500

    fix: fix unused variable

--- packages/nx-python/src/utils/versions/python-version.ts
+++ packages/nx-python/src/utils/versions/python-version.ts
@@ -1,15 +1,15 @@
 export function pythonToInformalVersion(pythonVersion: string): string {
-    if (!pythonVersion) {
-        return ""
-    }
-    const [major, minor, patch] = pythonVersion.split('.').map(Number);
-    const customFormat = `py${major}${minor}`;
-    return customFormat;
+  if (!pythonVersion) {
+    return '';
+  }
+  const [major, minor] = pythonVersion.split('.').map(Number);
+  const customFormat = `py${major}${minor}`;
+  return customFormat;
 }
 
 export function minimalPythonVersionFromRange(pythonVersion: string): string {
-    const cleanedVersion = pythonVersion.replace(' ', '').replace(/[>=<]/g, '');
-    const supported = cleanedVersion.split(",")
-    const version = supported.find((item) => item !== null) || null;
-    return version
+  const cleanedVersion = pythonVersion.replace(' ', '').replace(/[>=<]/g, '');
+  const supported = cleanedVersion.split(',');
+  const version = supported.find((item) => item !== null) || null;
+  return version;
 }
